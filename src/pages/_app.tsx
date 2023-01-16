8ac143aa720ae68d5d6a7b44e49ea050a30e7dfc
sephclt <josephajero22@gmail.com>
2023-01-16 09:53:03 +0800

    recoded

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,6 @@
 import type { AppProps } from 'next/app';
-import { SearchContextProvider } from '../contexts/searchcontext';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <SearchContextProvider>
-      <Component {...pageProps} />
-    </SearchContextProvider>
-  );
+  return <Component {...pageProps} />;
 }
